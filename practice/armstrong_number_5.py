9fa53f0a8aefa9255f56eb28f4c6a7924f9cb0ea
srinadh chintakindi <srinadhc07@gmail.com>
2026-08-18 10:21:50 +0530

    feat: added armstrong number

--- practice/armstrong_number_5.py
+++ practice/armstrong_number_5.py
@@ -3,4 +3,18 @@
 
 class ArmStrong():
     def armstrong(self, number):
-        pass
+        original = number
+        count = len(str(number))
+        sum = 0
+        while number > 0:
+            digit = number % 10
+            number = number // 10
+            sum += digit ** count
+        print("is ArmStrong number: True") if original == sum else print("is ArmStrong number: False")
+
+
+if __name__ == "__main__":
+    value = int(input("Enter value: "))
+    obj  = ArmStrong()
+    obj.armstrong(value)
+
